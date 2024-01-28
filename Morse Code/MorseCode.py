41946ce81a760d03e0e0776c6d329128c3d41ceb
Estefany Hernández Castro <153030320+EstefanyHdzCastro@users.noreply.github.com>
2024-01-28 12:43:03 -0600

    Add files via upload
    A text-based Python program to convert Strings into Morse Code

---
+++ Morse Code/MorseCode.py
@@ -0,0 +1,43 @@
+"""
+Programmer: Estefany Hernández
+Description: A text-based Python program to convert strings into Morse code
+"""
+
+# Morse code dictionary
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', '\'': '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...',
+    ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
+}
+
+def text_to_morse(text):
+    """
+    Convert a string into Morse code
+
+    Args:
+        text (str): The input text to be converted
+
+    Returns:
+        str: The Morse code equivalent of the input text
+    """
+    morse_code = ''
+    for char in text:
+        if char.upper() in MORSE_CODE_DICT:
+            morse_code += MORSE_CODE_DICT[char.upper()] + ' '
+        else:
+            morse_code += ' '
+    return morse_code.strip()
+
+def main():
+    """
+    Main function to run the program
+    """
+    text = input("Enter text to convert to Morse code: ")
+    morse_code = text_to_morse(text)
+    print("Morse Code:", morse_code)
+
+if __name__ == "__main__":
+    main()
